a20d62ee757da2ba37c4c81354726ae45b0ef39e
Lamont <viniciusmessias52@gmail.com>
2024-06-16 17:04:22 -0300

    First Version

---
+++ fechamentoDiarioProducao.py
@@ -0,0 +1,53 @@
+relatorio = """Bom dia,
+FECHAMENTO DIÁRIO DE PRODUÇÃO UNIDADE: MARINGÁ
+ DATA 13/06/2024
+
+COSTURA:
+
+ 1.QT OPERADORAS:
+
+ 2.PEÇAS PRODUZIDAS:
+
+ 3.PEÇAS ACUMULADAS:
+
+ 4.MINUTOS PRODUZIDOS:
+
+ 5.EFICIÊNCIA:
+
+ 6.EFICIÊNCIA ACUMULADA:
+
+ 7.PRODUTIVIDADE:
+
+ 8.PRODUTIVIDADE ACUMULADA:
+
+RELATÓRIO DOS GRUPOS:
+
+ GRUPO 01: Local 10 - 3 Pessoas - Tecido Plano - P: % - E: % - PEÇAS
+  - OBS: 
+
+ GRUPO 02: Local 423 - 4 Pessoas - Praia - P: %- E: % - PEÇAS
+  - OBS:
+
+ GRUPO 03: Local 12 - 4 Pessoas - P: % - E: % - PEÇAS
+  - OBS:
+
+ GRUPO 04: Local 16 - 4 Pessoas - Tecido Plano - P: % - E: % - PEÇAS
+  - OBS:
+
+ GRUPO 05: Local 14 - 4 Pessoas - Skin Fit - P: % - E: % - PEÇAS
+  - OBS:
+
+ GRUPO 06: Local 11 - 4 Pessoas - Skin Fit - P: % - E: % - PEÇAS
+  - OBS:
+
+ GRUPO 07: Local 424 - 5 Pessoas - Skin Fit - P: % - E: % - PEÇAS
+  - OBS:
+
+ ÍNDICE DE RETRABALHO: %
+
+ACABAMENTO:
+ TRAVETE: Local 25 - P: % - E: % - PEÇAS
+ REVISÃO: Local 27 - P: % - E: % - PEÇAS
+ EMBALAGEM: Local 28 - P % - E: % - PEÇAS
+"""
+print(relatorio)
